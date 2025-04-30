82785e561112a68959819bf1a87308863716af7f
marinkres <131777129+marinkres@users.noreply.github.com>
2025-04-30 16:25:39 +0200

    new post

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,7 @@
 import type { IconMap, SocialLink, Site } from '@/types'
 
 export const SITE: Site = {
-  title: '>marink.me<',
+  title: '>marink.me',
   description:
     'Marink Blog',
   href: 'https://marink.me',
